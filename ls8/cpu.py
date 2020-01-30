ff4226b490bfe81b0a5ccb3ddf8fc42606c71e7d
Adetunji <Ashennaike@gmail.com>
2020-01-29 19:57:17 -0500

    fixed the ls8 to work with file input

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -24,28 +24,37 @@ class CPU:
       return self.RAM[address]
 
     def ram_write(self, address, write):
-      self.RAM[address] = write
+      self.RAM[address] = int(f'0b{write}', 2)
 
-    def load(self):
+    def load(self, file):
         """Load a program into memory."""
 
         address = 0
 
+        f = open(file, 'r')
+        for line in f.readlines():
+          split = line.split('#')
+          instruction = split[0].strip()
+          if instruction == '':
+            continue
+          self.ram_write(address, instruction)
+          address += 1
+
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
-        for instruction in program:
-            self.RAM[address] = instruction
-            address += 1
+        # for instruction in program:
+        #     self.RAM[address] = instruction
+        #     address += 1
 
 
     def alu(self, op, reg_a, reg_b):
